c247469f85d65e1dae88be2816f8a2be23c1712c
James Davies <hello@jamesdevelops.co.uk>
2018-10-06 12:12:04 +0100

    Master

---
+++ themesass/home-page-template.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Template Name: Home Page
+ * Description: Home Page
+ *
+ * @package WordPress
+ * @subpackage themename
+ */
+
+get_header(); ?>
+
+    
+
+
+
+
+
+
+
+
+
+
+<?php get_footer(); ?>
